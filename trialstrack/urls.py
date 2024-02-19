257471244b76db8abec564b361e6f1034d114bbf
Kirthana Sukumaran <kirthanaa07@gmail.com>
2024-02-18 22:17:01 -0600

    CRUD on location done

--- trialstrack/urls.py
+++ trialstrack/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from trialstrackapi.views import LocationView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r"locations", LocationView, "location")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
---
+++ trialstrackapi/views/location_view.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from http import HTTPMethod
+from rest_framework import serializers, status
+from trialstrackapi.models import Location, User
+
+class LocationView(ViewSet):
+  def retrieve(self, request, pk):
+    try:
+      location = Location.objects.get(pk=pk)
+      serializer = LocationSerializer(location)
+      return Response(serializer.data)
+    except Location.DoesNotExist as ex:
+      return Response({"message":ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+  def list(self, request):
+    location = Location.objects.all()
+    serializer = LocationSerializer(location, many=True)
+    return Response(serializer.data)
+  
+  def create(self, request):
+    user = User.objects.get(uid=request.data["userId"])
+    location = Location.objects.create(
+      name=request.data["name"],
+      address=request.data["address"],
+      city=request.data["city"],
+      state=request.data["state"],
+      zip=request.data["zip"],
+      country=request.data["country"],
+      user=user,
+    )
+    serializer = LocationSerializer(location)
+    return Response(serializer.data)
+  
+  def update(self, request, pk):
+    location = Location.objects.get(pk=pk)
+    location.name = request.data["name"]
+    location.address = request.data["address"]
+    location.city = request.data["city"]
+    location.state = request.data["state"]
+    location.zip = request.data["zip"]
+    location.country = request.data["country"]
+    user = User.objects.get(pk=request.data["userId"])
+    location.user = user
+    location.save()
+    
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+  def delete(self, request, pk):
+    location = Location.objects.get(pk=pk)
+    location.delete()
+    return Response(None, status=status.HTTP_204_NO_CONTENT)
+  
+class LocationSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Location
+    fields = ("id", "user", "name", "address", "city", "state", "zip", "country")  
+      
+    
